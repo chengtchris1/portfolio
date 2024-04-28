a5b0360981c483a0470fc68c9b1ed860c8fe254c
chengtchris1 <152359315+chengtchris1@users.noreply.github.com>
2024-04-28 13:58:08 -0700

    animation

--- src/App.jsx
+++ src/App.jsx
@@ -20,18 +20,20 @@ function App() {
         <Hero />
         <div className="absolute inset-0 z-10 bg-transparent sm:hidden"></div>
       </Parallax>
-      <Parallax speed={-10} opacity={[3, 0.5]} easing={"easeInOut"}>
+      <Parallax speed={-10} opacity={[3, 0]} easing={"easeInOut"}>
         <div className="mx-0 sm:mx-[8.5rem]">
           <About />
         </div>
       </Parallax>
-      <br /> <br />
-      <Parallax speed={-2} opacity={[3, 0]} easing={"easeInOut"}>
-        <div className="mx-10 my-12 flex items-center justify-center text-left text-white sm:text-center">
+      <Parallax speed={0} opacity={[3, 0]} easing={"easeInOut"}>
+        <br /> <br />
+        <div className="mx-10 mb-1 mt-12 flex items-center justify-center text-left text-white sm:text-center">
           <h2 className="text-6xl">Some technologies I've used...</h2>
         </div>
       </Parallax>
-      <TechStack />
+      <Parallax speed={15}>
+        <TechStack />
+      </Parallax>
       <Portfolio />
     </div>
   );
